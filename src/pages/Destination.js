1c640bf27c12595b547b1e9b52b98fe5b32aad5e
Dewaley <adewaleademilua@gmail.com>
2022-05-03 12:34:13 +0100

    Destination desktop

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -22,15 +22,15 @@ const Destination = ({ destinations }) => {
   
   return (
     <div className='text-white destination-container min-h-screen overflow-x-hidden flex justify-center items-center'>
-      <div className='flex md:justify-end flex-col px-4 pt-24 w-full max-w-lg md:max-w-none'>
+      <div className='flex md:justify-end flex-col px-4 md:px-12 pt-28 w-full max-w-lg md:max-w-none'>
         <h1 className='tracking-widest'>
           <span>0{index + 1}</span>&nbsp; PICK YOUR DESTINATION
         </h1>
-        <div className='flex flex-col items-center'>
-          <div className='pt-16'>
+        <div className='flex flex-col items-center md:flex-row md:gap-x-2'>
+          <div className='pt-10 md:w-2/4'>
             <img src={imageIndex(index)} alt='' />
           </div>
-          <div className='flex items-center justify-center flex-col pt-16 text-lg'>
+          <div className='flex items-center justify-center flex-col pt-10 text-lg md:w-2/4'>
             <div className='flex gap-x-4'>
               <button
                 onClick={() => setIndex(0)}
@@ -69,16 +69,16 @@ const Destination = ({ destinations }) => {
             <p className='text-lg pb-12 border-b-2 border-b-white'>
               {destinations[index].description}
             </p>
-            <div className='flex flex-col text-center'>
+            <div className='flex flex-col text-center md:flex-row md:gap-x-6'>
               <div className='pt-4'>
-                <p>AVG. DISTANCE</p>
-                <p className='text-3xl uppercase'>
+                <p className='font-thin'>AVG. DISTANCE</p>
+                <p className='text-xl uppercase'>
                   {destinations[index].distance}
                 </p>
               </div>
               <div className='pt-4'>
-                <p>EST. TRAVEL TIME</p>
-                <p className='text-3xl uppercase'>
+                <p className='font-thin'>EST. TRAVEL TIME</p>
+                <p className='text-xl uppercase'>
                   {destinations[index].travel}
                 </p>
               </div>
